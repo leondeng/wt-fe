c2af9953e083379060e12244415c8e339019b351
Fan Deng <fan.deng@momentumcloud.com.au>
2015-07-24 13:34:01 +1000

    project init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wtFeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the wtFeApp
+ */
+angular.module('wtFeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wtFeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wtFeApp
+ */
+angular.module('wtFeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
