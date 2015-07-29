735b1b61844bfc61f304a9f77e7712655afbb7a5
Fan Deng <leondeng@gmail.com>
2015-07-29 23:33:31 +1000

    try to load items for page number, still buggy

--- app/scripts/controllers/history.js
+++ app/scripts/controllers/history.js
@@ -9,24 +9,29 @@
  */
 angular.module('wtFeApp')
   .controller('HistoryCtrl', function ($scope, trackFactory) {
+    $scope.currentPage = 1;
+    $scope.loadingIsDone = false;
+    $scope.maxSize = 5;
+    $scope.bigTotalItems = 175;
+    $scope.bigCurrentPage = 1;
 
-    trackFactory.getTracksFor()
-    .then(function (history){
-      $scope.tracks = history.tracks;
-      $scope.totalItems = history.pagination.count;
-      $scope.currentPage = 1;
-      $scope.loadingIsDone = true;
+    $scope.setPage = function (pageNo) {
+      $scope.currentPage = pageNo;
+    };
 
-      $scope.setPage = function (pageNo) {
-        $scope.currentPage = pageNo;
-      };
+    $scope.pageChanged = function() {
+      console.log('Page changed to: ' + $scope.currentPage);
+    };
 
-      $scope.pageChanged = function() {
-        $log.log('Page changed to: ' + $scope.currentPage);
-      };
+    $scope.loadItemsForPage = function(pageNo) {
+      trackFactory.getTracksFor(pageNo)
+      .then(function (history) {
+        $scope.tracks = history.tracks;
+        $scope.totalItems = history.pagination.total;
+        $scope.currentPage = history.pagination.current;
+        $scope.loadingIsDone = true;
+      });
+    };
 
-      $scope.maxSize = 5;
-      $scope.bigTotalItems = 175;
-      $scope.bigCurrentPage = 1;
-    });
+    $scope.loadItemsForPage($scope.currentPage);
   });
