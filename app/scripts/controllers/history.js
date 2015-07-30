73cca6465518586b9bc86915cb3fa5d8d51d2c7a
Fan Deng <leondeng@gmail.com>
2015-07-30 23:32:09 +1000

    develop: currentPage not changed?

--- app/scripts/controllers/history.js
+++ app/scripts/controllers/history.js
@@ -9,29 +9,30 @@
  */
 angular.module('wtFeApp')
   .controller('HistoryCtrl', function ($scope, trackFactory) {
+
     $scope.currentPage = 1;
-    $scope.loadingIsDone = false;
     $scope.maxSize = 5;
     $scope.bigTotalItems = 175;
     $scope.bigCurrentPage = 1;
 
-    $scope.setPage = function (pageNo) {
-      $scope.currentPage = pageNo;
-    };
+    $scope.tracks = [];
+    $scope.loadingIsDone = false;
+
+    loadTracks();
+    $scope.loadingIsDone = true;
 
     $scope.pageChanged = function() {
       console.log('Page changed to: ' + $scope.currentPage);
+      loadTracks();
     };
 
-    $scope.loadItemsForPage = function(pageNo) {
-      trackFactory.getTracksFor(pageNo)
-      .then(function (history) {
-        $scope.tracks = history.tracks;
-        $scope.totalItems = history.pagination.total;
-        $scope.currentPage = history.pagination.current;
-        $scope.loadingIsDone = true;
-      });
+    function loadTracks() {
+      trackFactory.getTracksFor($scope.currentPage)
+        .then(function (history) {
+          //$scope.tracks = history.tracks;
+          $scope.totalItems = history.pagination.total;
+          angular.copy(history.tracks, $scope.tracks);
+          //$scope.loadingIsDone = true;
+        });
     };
-
-    $scope.loadItemsForPage($scope.currentPage);
   });
